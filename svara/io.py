6e5232dea63c368a06200ca97fe01392b719e012
Rong Shengxue <300581293+abenneto@users.noreply.github.com>
2023-11-21 16:15:17 +0800

    feat(io): read wav files

---
+++ svara/io.py
@@ -0,0 +1,54 @@
+"""音频读写与重采样。
+
+底层直接使用 :func:`scipy.io.wavfile`，只支持 WAV。整数 PCM 会被归一化到
+``[-1, 1)`` 的 ``float64``，多声道默认混成单声道。
+"""
+
+from __future__ import annotations
+
+from pathlib import Path
+
+import numpy as np
+from scipy.io import wavfile
+
+from svara.exceptions import AudioIOError
+from svara.utils import FloatArray
+
+# 各整数 PCM 位宽对应的归一化除数。
+_INT_SCALE = {
+    np.dtype(np.int16): 2**15,
+    np.dtype(np.int32): 2**31,
+    np.dtype(np.uint8): 2**7,  # uint8 会先减去偏置
+}
+
+
+def read_wav(path: str | Path, mono: bool = True) -> tuple[FloatArray, int]:
+    """读取 WAV 文件，返回 ``(signal, sample_rate)``。
+
+    信号被转换为 ``float64`` 并归一化到 ``[-1, 1)``。当 ``mono=True`` 且文件为
+    多声道时，取各声道的平均值。
+    """
+    try:
+        sample_rate, data = wavfile.read(str(path))
+    except (OSError, ValueError) as exc:  # pragma: no cover - 依赖具体文件
+        raise AudioIOError(f"无法读取音频文件：{path}") from exc
+
+    signal = _to_float(data)
+    if signal.ndim == 2 and mono:
+        signal = signal.mean(axis=1)
+    return np.ascontiguousarray(signal), int(sample_rate)
+
+
+def _to_float(data: np.ndarray) -> FloatArray:
+    """把原始 PCM 数据转换为归一化的 ``float64``。"""
+    if np.issubdtype(data.dtype, np.floating):
+        return data.astype(np.float64, copy=False)
+
+    dtype = data.dtype
+    if dtype == np.dtype(np.uint8):
+        return (data.astype(np.float64) - 128.0) / _INT_SCALE[dtype]
+
+    scale = _INT_SCALE.get(dtype)
+    if scale is None:
+        raise AudioIOError(f"不支持的 PCM 位宽：{dtype}")
+    return data.astype(np.float64) / scale
